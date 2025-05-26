42bcd180262528e2929030f5721ac3cc8b846da1
agolovachev <antongolova@gmail.com>
2025-05-27 00:30:52 +0700

    [server/memory-bank]: refactor - унификация именования файлов и упрощение архитектуры

---
+++ bitcoin-moon/server/src/controllers/index.js
@@ -0,0 +1,14 @@
+/**
+ * Индексный файл для экспорта всех контроллеров
+ */
+const bitcoinController = require('./bitcoinController');
+const moonController = require('./moonController');
+const astroController = require('./astroController');
+const eventsController = require('./eventsController');
+
+module.exports = {
+  bitcoinController,
+  moonController,
+  astroController,
+  eventsController
+}; 
